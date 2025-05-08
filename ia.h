fdee5bc7aa133d267458a458325b72df0a4a64eb
leMecanicien-beep <anesmokrane05@gmail.com>
2025-05-08 22:58:00 +0200

    Create ia.h

---
+++ ia.h
@@ -0,0 +1,9 @@
+#ifndef IA_H
+#define IA_H
+
+#include "equipe.h"
+
+// L’IA joue son tour : elle choisit un combattant, une action, et une cible
+void jouerTourIA(Equipe* equipeIA, Equipe* equipeAdverse);
+
+#endif
