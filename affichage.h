0105c917ce9ecf282de463f09a1bb718f05b3a8f
leMecanicien-beep <anesmokrane05@gmail.com>
2025-05-09 20:10:29 +0200

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -4,5 +4,7 @@
 #include "equipe.h"
 
 void afficherEquipesCoteACote(Equipe* equipeGauche, Equipe* equipeDroite);
+void afficherCarteCombattant(Combattant* c);
+void afficherChoixEquipe(Combattant* tous, int nb);
 
 #endif
