05e4147f746f0363f0e1a4110c683a997259b1b3
leMecanicien-beep <anesmokrane05@gmail.com>
2025-05-09 16:12:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utilitaire.h"
+#include "fichier.h"
+#include "equipe.h"
+#include "combat.h"
+#include "ia.h"
+#include "combattant.h"
+
+
 int main() {
     initialiserAleatoire();
 
