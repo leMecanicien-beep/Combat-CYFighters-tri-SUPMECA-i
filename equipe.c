affa5d0fab8af3b95b86f58c868399addb788902
leMecanicien-beep <anesmokrane05@gmail.com>
2025-05-10 19:03:34 +0200

    Update equipe.c

--- equipe.c
+++ equipe.c
@@ -6,7 +6,8 @@
 // Initialise une équipe avec un nom
 void initialiserEquipe(Equipe *e, const char *nom) {
     if(e == NULL || nom == NULL){
-        
+        exit(130);
+    }
     strncpy(e->nom, nom, 29);
     e->nom[29] = '\0';
     e->nbCombattants = 0;
@@ -14,6 +15,9 @@ void initialiserEquipe(Equipe *e, const char *nom) {
 
 // Ajoute un combattant à l’équipe
 void ajouterCombattant(Equipe *e, Combattant *c) {
+    if(e == NULL || c == NULL){
+        exit(140);
+    }
     if (e->nbCombattants < NB_EQUIPE_MAX) {
         e->combattants[e->nbCombattants] = c;
         e->nbCombattants++;
@@ -24,6 +28,9 @@ void ajouterCombattant(Equipe *e, Combattant *c) {
 
 // Renvoie 1 si tous les combattants sont KO, sinon 0
 int equipeEstKO(Equipe *e) {
+    if(e == NULL){
+        exit(150);
+    }
     int tousKO = 1;
     for (int i = 0; i < e->nbCombattants; i++) {
         if (e->combattants[i]->pv > 0) {
