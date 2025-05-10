edea0de4e9cc94841d0507edaa9a3963a38ea7de
leMecanicien-beep <anesmokrane05@gmail.com>
2025-05-10 17:23:52 +0200

    Update MAIN.c

--- MAIN.c
+++ MAIN.c
@@ -1,11 +1,10 @@
-
-
+#include <stdio.h>
 
 int main() {
   printf("\n========================================\n");
   printf("||           🔥 FIN DU COMBAT 🔥           ||\n");
   printf("========================================\n");
-  printf("🎉 Félicitations, l’équipe %s remporte la victoire ! 🎉\n", nomEquipe);
+  printf("🎉 Félicitations, l’équipe remporte la victoire ! 🎉\n");
   printf("   Vous avez vaincu l’équipe adverse avec honneur.\n");
   printf("💀 L’IA a triomphé ! Mieux vaut retenter votre chance... 💀\n");
   printf("   Courage, l’honneur est dans l’effort !\n");
